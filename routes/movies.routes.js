2dcd1a021a495d5b1b469a411b38a9fbd1a14ed4
Soufiane Bdaoui <soufianebdaoui@gmail.com>
2022-08-15 20:37:15 +0200

    it 9 done

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,16 +1,62 @@
 const express = require("express");
 const router = express.Router();
 const Movie = require("../models/Movie.model");
+const Celebrity = require("../models/Celebrity.model");
+const { populate } = require("../models/Celebrity.model");
 
 
 router.get('/create', (req, res) =>{
-    res.render("movies/new-movie")
+    Celebrity.find()
+        .then(response => {
+            res.render("movies/new-movie", {response} );
+        })
+        .catch(error => {console.log("there is an error ", error)})
 })
 
 router.post("/create", (req, res) =>{
     const filmData = req.body;
 
-    
+    Movie.create(filmData)
+        .then(response =>{
+            res.redirect("movies");
+        })
+        .catch(error => {console.log("there is an error ", error)})
+
+
+})
+
+router.get("/movies", (req, res)=>{
+    Movie.find()
+        .then(response =>{
+            res.render("movies/movies", {response});
+        })
+        .catch(error => {console.log("there is an error ", error)})
+
+})
+
+router.get("/:id", (req, res) =>{
+    const {id} = req.params;
+    Movie.findById(id)
+        .then(response =>{
+           response.populate("cast") 
+           .then(response =>{
+                res.render("./movies/movie-details", {response});
+            })
+            .catch(error => { console.log("populating gave an error ", error)})
+        })
+        .catch(error =>{console.log("did not manage to find the required set of data, here is the error ", error)})
+})
+
+
+router.post("/:id/delete", (req, res) =>{
+    const {id} = req.params;
+    console.log(id);
+
+    Movie.findByIdAndDelete(id)
+        .then(response =>{
+            res.redirect("../movies");
+        })
+        .catch(error =>{ console.log("this is the error: ", error)})
 })
 
 module.exports = router;
